af57c898f3a4d9ffcbae740a5a027a3415076222
Aniket Potdar <aniketpanditpotdar@gmail.com>
2023-08-20 14:26:33 +0530

    updated the table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex ">
+      <table
+        className="table table-striped table-bordered"
+        style={{ width: "1%" }}
+      >
+        <thead>
+          <tr>
+            <th className="table-dark">Project</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Padu</th>
+          </tr>
+          <tr>
+            <th>Sanskar</th>
+          </tr>
+          <tr>
+            <th>Yash</th>
+          </tr>
+          <tr>
+            <th>Aniket</th>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="table-responsive">
+        <table className="table table-striped table-bordered mb-auto">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Html,CSS,JS Crud
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With Localstorage
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With Rest API
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With Angular
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With React
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With MEAN and SQL/NoSQL
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With MERN and SQL/NoSQL
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With MEAN(Ionic) and SQL/NoSQL
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Crud With MERN(Native) and SQL/NoSQL
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Java Springboot Hybernate Crud with SQL/NoSQL
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                PHP with Larvel Crud with SQL/NoSQL
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Python with Django Crud with SQL/NoSQL
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                DSA with C
+              </th>
+              <th
+                scope="col"
+                className={`dot_txt ${"hover-ellipsis"} table-dark`}
+              >
+                Oops with C++
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="text-center">
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+            </tr>
+            <tr className="text-center">
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+            </tr>
+            <tr className="text-center">
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">✔</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+            </tr>
+            <tr className="text-center">
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+              <td scope="col">--</td>
+            </tr>{" "}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
